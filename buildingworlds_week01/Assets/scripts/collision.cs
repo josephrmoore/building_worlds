919d5ca8c9ace174330bd53124c6179158fb6a03
josephrmoore <josephrmoore@gmail.com>
2014-02-20 02:17:46 -0500

    adding color paint

---
+++ buildingworlds_week01/Assets/scripts/collision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int buffer = 10;
+
+		float x, y, z;
+		x = Camera.main.gameObject.transform.position.x;
+		y = Camera.main.gameObject.transform.position.y;
+		z = Camera.main.gameObject.transform.position.z;
+
+		int obj_x = -43;
+		int obj_y = 62;
+		int obj_z = -29; 
+
+
+		if(x > (obj_x-buffer) && x < (obj_x+buffer)){
+			if(y > (obj_y-buffer) && y < (obj_y+buffer)){
+				if(z > (obj_z-buffer) && z < (obj_z+buffer)){
+
+					if(Input.GetKeyDown(KeyCode.Y)){
+						Camera.main.gameObject.transform.position = new Vector3 (-790, 534, -70);
+					}
+				}
+			}
+		}
+	}
+}
